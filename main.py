aaf34a3dea2c30342ce0574c1c208f383228dce0
tudemaha <gede_mahardika88@yahoo.co.id>
2022-12-31 02:54:00 +0800

    make main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # import the web view
 from view.web import run_streamlit
-from mockups.cfg_to_cnf import cfg_to_cnf
 
 # run run_streamlit() function for the first time use
 if __name__ == "__main__":
-    # run_streamlit()
-    cfg_to_cnf()
+    run_streamlit()
