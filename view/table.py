1efa03450b29949fd7b5e3aa5a8b8f03ef2b86f8
tudemaha <gede_mahardika88@yahoo.co.id>
2023-01-01 20:03:29 +0800

    add comments in table.py

--- view/table.py
+++ view/table.py
@@ -1,4 +1,6 @@
+# function to create table in html
 def print_table(table_data, column_name):
+    # prepare the table's style
     table = """
     <style>
     table {
@@ -13,18 +15,30 @@ def print_table(table_data, column_name):
     </style>
     """
 
+    # <table> opening tag
     table += "<table>"
 
+    # create row for header
     table += "<tr>"
+    # iterate words
     for name in column_name:
+        # add word to the header
         table += f"<th>{name}</th>"
+    # row closing tag
     table += "</tr>"
 
+    # for every row in list
     for row in table_data:
+        # create row for data
         table += "<tr>"
+        # for every colum in row
         for column in row:
+            # create column
             table += f"<td>{column}</td>"
+        # row closing tag
         table += "</tr>"
+    # table closing tag
     table += "</table><br>"
 
+    # return the html table
     return table
